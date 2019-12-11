ac0d2ef978bb0c25d6f84001cc59cadc1af511e9
ismailbennani <ismail.lahkim.bennani@ens.fr>
2019-12-11 11:10:25 +0100

    removed unnecessary files

--- src/fadbad_utils.ml
+++
@@ -1,4 +0,0 @@
-let user_assert b s =
-  if not b then begin Printf.eprintf "User assertion failed: %s\n" s; exit 1 end
-let internal_assert b s =
-  if not b then begin Printf.eprintf "Internal error: %s\n" s; exit 1 end
